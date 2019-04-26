b309c53e42d5b30ce27aa5afe040f3553547661d
Davidcode1976 <dw751126@sina.cn>
2019-04-26 23:46:50 +0800

    修改后

--- findwords.py
+++ findwords.py
@@ -3,7 +3,7 @@ import re
 
 with open('from.txt','r',encoding='utf-8')as f:
     text=f.read()
-words=re.findall(r'\b[a-zA-Z]+[a-zA-Z]+\b',text)
+words=re.findall(r'\b[a-zA-Z]+\b',text)
 words.sort()
 
 #print(words)
