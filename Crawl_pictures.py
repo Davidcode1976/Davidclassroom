43bc3809e7cbb1db46dbf6ce2c0ab73674b1aa08
Davidcode1976 <dw751126@sina.cn>
2019-05-18 01:31:11 +0800

    Crawl_pictures

---
+++ Crawl_pictures.py
@@ -0,0 +1,67 @@
+# -*- coding:utf-8 -*-
+
+# 爬取糗百热图（https://www.qiushibaike.com/imgrank/）
+# 输入页码，抓取指定页面上的图片并保存到本地文件夹；
+# 采用多线程技术进行爬取，减少等待时间；
+# 可选要求：将程序打包为 exe 文件；
+
+import requests,re,threading,os
+
+
+def get_pic_url(i):
+    url='https://www.qiushibaike.com/imgrank/page/%d/'% i
+    req=requests.get(url)
+    data=req.text
+    pic_url_list=re.findall(r'//pic.qiushibaike.com/system/pictures\S*\.jpg',data)
+    return pic_url_list
+
+
+def download_pic(pic_url):
+    get_pic=requests.get('http:'+pic_url)
+    pic_file_path=r'pic_file\%s'% str(pic_url.split('/')[-1])
+    with open(pic_file_path,'wb')as f:
+        f.write(get_pic.content)
+    print('%s下载完成'%str(pic_url.split('/')[-1]))
+
+
+def main():
+    begin_page = int(input('请输入起始页: '))
+    end_page = int(input('请输入终止页：'))
+
+    if end_page >= begin_page:
+
+        print('开始下载')
+
+        headers = {'User-Agent': 'Chrome'}
+
+        isExist = os.path.exists('pic_file')
+        if not isExist:
+            os.makedirs('pic_file')
+
+        pic_url_list_set = []
+
+        for i in range(begin_page, end_page + 1):
+            pic_url_list = get_pic_url(i)
+            pic_url_list_set += pic_url_list
+
+        dl_threads = []
+
+        for i in range(len(pic_url_list_set)):
+            threads = threading.Thread(target=download_pic, args=(pic_url_list_set[i], ))
+            dl_threads.append(threads)
+
+        for i in range(len(pic_url_list_set)):
+            dl_threads[i].start()
+
+        for i in range(len(pic_url_list_set)):
+            dl_threads[i].join()
+
+        print('结束下载')
+
+    else:
+        print('输入有误，程序终止')
+
+if __name__=='__main__':
+    main()
+
+
